bd19d2d1ef8c1ae413f62aa67f7b66e22848f645
Decebel Geduriagao <Decebel Geduriagao>
2018-03-01 14:13:19 +0800

    pasted firebase credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDj7CL3lRWL1NTMmbH8beazm7JEmVDziPA",
+    authDomain: "angular-crypto-currency-41afc.firebaseapp.com",
+    databaseURL: "https://angular-crypto-currency-41afc.firebaseio.com",
+    projectId: "angular-crypto-currency-41afc",
+    storageBucket: "",
+    messagingSenderId: "944693164353"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDj7CL3lRWL1NTMmbH8beazm7JEmVDziPA",
+    authDomain: "angular-crypto-currency-41afc.firebaseapp.com",
+    databaseURL: "https://angular-crypto-currency-41afc.firebaseio.com",
+    projectId: "angular-crypto-currency-41afc",
+    storageBucket: "",
+    messagingSenderId: "944693164353"
+  }
 };
