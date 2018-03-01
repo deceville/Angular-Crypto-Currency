6dab9ac218eefbde687d0dbd0c9c811045540925
Decebel Geduriagao <Decebel Geduriagao>
2018-03-01 14:04:26 +0800

    added data service to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,16 @@ export class AppComponent {
   title = 'app';
   objectKeys = Object.keys;
   cryptos: any;
+
+  constructor(private _data: DataService) {
+    
+      }
+    
+      ngOnInit() {
+        this._data.getPrices()
+          .subscribe(res => {
+            this.cryptos = res;
+            //console.log(res);
+          });
+      }
 }
